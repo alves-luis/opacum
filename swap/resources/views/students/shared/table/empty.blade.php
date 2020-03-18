fde4de2114afd8441bb38676a04f742a2d20da6e
Luís Alves <luismig.alves@gmail.com>
2020-03-18 15:16:52 +0000

    Finish refactoring of repo

--- swap/resources/views/students/shared/table/empty.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="card-body p-5 text-center">
-    <h4 class="card-title"><small>Enrolled students will appear here.</small></h4>
-    <p class="card-text text-muted">When students enroll in this course, the details of enrollments will be presented here.</p>
-</div>
