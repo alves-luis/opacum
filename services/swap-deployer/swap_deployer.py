8067af8263110bec5e9d652fac31e6eac037649a
Luís Alves <luismig.alves@gmail.com>
2020-04-29 21:33:02 +0100

    Update swap-deployer to validate courses

--- services/swap-deployer/swap_deployer.py
+++ services/swap-deployer/swap_deployer.py
@@ -1,6 +1,15 @@
 from flask import Flask, abort, request
 from jinja2 import Template
 import docker
+import re
+
+"""
+Parameters
+"""
+REGISTRY_USERNAME = "alvesluis98"
+REGISTRY_PASSWORD = ""
+REGISTRY_URL = "alvesluis98/swap-deployer"
+DEFAULT_DOCKER_NETWORK = "sms-net"
 
 application = Flask(__name__)
 
@@ -47,8 +56,7 @@ def deploy(subdomain):
 
     net = create_network(docker_client, subdomain)
     postgres_service = setup_postgres_service(docker_client, subdomain)
-    swap_service = setup_swap_service(docker_client, subdomain)
-    #setup reverse proxy TODO
+    swap_service = setup_swap_service(docker_client, subdomain, deployment_dict)
     setup_reverse_proxy(docker_client, subdomain)
     return "Ok"
 
@@ -56,7 +64,6 @@ def deploy(subdomain):
 Setup reverse-proxy configuration
 """
 def setup_reverse_proxy(client, subdomain):
-    # Should verify the subdomain here
     nginx_template = Template(open('nginx_template.j2', 'r').read()).render(subdomain=subdomain)
     file = open(f"/opt/swap-deployer/sites/{subdomain}.conf", 'w')
     print(nginx_template, file=file)
@@ -65,18 +72,20 @@ def setup_reverse_proxy(client, subdomain):
     rp_service = client.services.get(rp_id)
     rp_service.force_update()
 
-
 """
 Create a new network, given a subdomain
 """
 def create_network(client, subdomain):
-    network_name = subdomain + "-net"
+    network_name = net_name(subdomain)
     net = client.networks.create(network_name, driver="overlay")
     return net
 
+"""
+Setup database configuration
+"""
 def setup_postgres_service(client, subdomain):
-    postgres_name = subdomain + "-db"
-    network_name = subdomain + "-net"
+    postgres_name = db_name(subdomain)
+    network_name = net_name(subdomain)
     envs = ["POSTGRES_USER=swapper", "POSTGRES_DB=swapper", "POSTGRES_PASSWORD=secret"]
     service = client.services.create("postgres:12", command=None,
         name=postgres_name,
@@ -84,21 +93,80 @@ def setup_postgres_service(client, subdomain):
         env=envs)
     return service
 
-def setup_swap_service(client, subdomain):
-    swap_name = subdomain + "-app"
-    network_name = subdomain + "-net"
+"""
+Setup Swap configuration
+"""
+def setup_swap_service(client, subdomain, config):
+    swap_name = app_name(subdomain)
+    network_name = net_name(subdomain)
+    setup_courses(config)
     image = client.images.build(dockerfile="Dockerfile",
         path="./swap",
-        tag=f"alvesluis98/swap-deployer:{subdomain}-swap",
-        buildargs={ "db_host": f"{subdomain}-db" })
-    client.login(username='alvesluis98', password='secret') # should be stored in secret
-    client.images.push(f'alvesluis98/swap-deployer:{subdomain}-swap')
-    service = client.services.create(f"alvesluis98/swap-deployer:{subdomain}-swap", command=None,
+        tag=f"{REGISTRY_URL}:{image_name(subdomain)}",
+        buildargs={ "db_host": db_name(subdomain) })
+    client.login(username=REGISTRY_USERNAME, password=REGISTRY_PASSWORD)
+    client.images.push(f"{REGISTRY_URL}:{image_name(subdomain)}")
+    service = client.services.create(f"{REGISTRY_URL}:{image_name(subdomain)}", command=None,
         name=swap_name,
-        networks=[network_name, "sms-net"]
+        networks=[network_name, DEFAULT_DOCKER_NETWORK]
     )
     return service
 
+"""
+Setup courses in the Swap image to be built
+"""
+def setup_courses(config):
+    courses = config.get('courses')
+    for course in courses:
+        if not valid_course(course):
+            return "Invalid Course"
+    
+    template = Template(open('CoursesTableSeeder_template.j2', 'r').read()).render(courses=courses)
+    file = open(f"./swap/swap/database/seeds/CoursesTableSeeder.php", 'w')
+    print(template, file=file)
+
+
+"""
+Given a course, validates its fields ["code", "name", "semester", "year"]
+"""
+def valid_course(course):
+    valid_course_code(course["code"])
+
+def valid_course_code(code):
+    return bool(re.match(r"^[\w0-9]+$", code))
+
+def valid_course_name(name):
+    return bool(re.match(r"^[\w ]+$", name))
+
+def valid_course_semester(semester):
+    return isinstance(semester, int) and semester >= 0
+
+def valid_course_year(year):
+    return isinstance(year, int) and year >= 0
+
+"""
+Given a subdomain, returns the name that the Swap Network should have
+"""
+def net_name(subdomain):
+    return f"{subdomain}-net"
+
+"""
+Given a subdomain, returns the name that the Swap service should have
+"""
+def app_name(subdomain):
+    return f"{subdomain}-app"
+
+"""
+Given a subdomain, returns the name that the Swap Database should have
+"""
+def db_name(subdomain):
+    return f"{subdomain}-db"
+
+"""
+Given a subdomain, returns the name of the image that should be stored in the image registry
+"""
+def image_name(subdomain):
+    return f"{subdomain}-swap"
 
 if __name__ == '__main__':
     application.run(debug=True, host='0.0.0.0')
